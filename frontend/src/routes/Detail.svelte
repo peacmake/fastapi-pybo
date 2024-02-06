f5854fee4efaa3f00457ff279e42d0be58f84957
peacmake <peacmake@gmail.com>
2024-02-06 22:55:49 +0900

    3th commit

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -61,6 +61,23 @@
             );
         }
     }
+
+    function delete_answer(answer_id) {
+        if(window.confirm('정말로 삭제하시겠습니까?')) {
+            let url = "/api/answer/delete"
+            let params = {
+                answer_id: answer_id
+            }
+            fastapi('delete', url, params, 
+                (json) => {
+                    get_question()
+                },
+                (err_json) => {
+                    error = err_json
+                }
+            )
+        }
+    }
 </script>
 
 <div class="container my-3">
@@ -72,6 +89,12 @@
                 {question.content}
             </div>
             <div class="d-flex justify-content-end">
+                {#if question.modify_date }
+                <div class="badge bg-light text-dark p-2 text-start mx-3">
+                    <div class="mb-2">modified at</div>
+                    <div>{moment(question.modify_date).format("YYYY년 MM월 DD일 hh:mm a")}</div>
+                </div>
+                {/if}
                 <div class="badge bg-light text-dark p-2 text-start">
                     <div class="mb-2">
                         {question.user ? question.user.username : ""}
@@ -118,6 +141,12 @@
                     {answer.content}
                 </div>
                 <div class="d-flex justify-content-end">
+                    {#if answer.modify_date }
+                    <div class="badge bg-light text-dark p-2 text-start mx-3">
+                        <div class="mb-2">modified at</div>
+                        <div>{moment(answer.modify_date).format("YYYY년 MM월 DD일 hh:mm a")}</div>
+                    </div>
+                    {/if}
                     <div class="badge bg-light text-dark p-2 text-start">
                         <div class="mb-2">
                             {answer.user ? answer.user.username : ""}
@@ -128,6 +157,12 @@
                             )}
                         </div>
                     </div>
+                </div>
+                <div class="my-3">
+                    {#if answer.user && $username === answer.user.username }
+                        <a use:link href="/answer-modify/{answer.id}" class="btn btn-sm btn-outline-secondary">수정</a>
+                        <button class="btn btn-sm btn-outline-secondary" on:click={() => delete_answer(answer.id) }>삭제</button>
+                    {/if}
                 </div>
             </div>
         </div>
